b4a414ca0023aaa3c896ec3d2c408121c838c3fa
Apna-project2003 <214593185+Apna-project2003@users.noreply.github.com>
2025-07-16 20:17:03 +0530

    Backend

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ import { connection } from "./database/connection.js";
 import { errorMiddleware } from "./middlewares/error.js";
 import userRouter from "./router/userRoutes.js";
  import auctionItemRouter from "./router/auctionItemRoutes.js";
-// import bidRouter from "./router/bidRoutes.js";
+ import bidRouter from "./router/bidRoutes.js";
 // import commissionRouter from "./router/commissionRouter.js";
 // import superAdminRouter from "./router/superAdminRoutes.js";
 // import { endedAuctionCron } from "./automation/endedAuctionCron.js";
@@ -38,7 +38,7 @@ app.use(
 
  app.use("/api/v1/user", userRouter);
  app.use("/api/v1/auctionitem", auctionItemRouter);
-// app.use("/api/v1/bid", bidRouter);
+ app.use("/api/v1/bid", bidRouter);
 // app.use("/api/v1/commission", commissionRouter);
 // app.use("/api/v1/superadmin", superAdminRouter);
 
